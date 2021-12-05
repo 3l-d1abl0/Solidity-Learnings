c5c1736ebee9f323a1b2ff0b1589278c70143ca3
sameer <sameer.barha12@gmail.com>
2021-12-05 12:58:20 +0530

    using libraries in Solidity

---
+++ 14.1-using-library.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+import"https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract Bank{
+
+    mapping(address => uint) public accounts;
+    using SafeMath for uint256;
+
+
+
+    function deposit() public payable{
+
+        require(accounts[msg.sender] + msg.value >= accounts[msg.sender], "Overflow Error");
+
+        accounts[msg.sender] = accounts[msg.sender].add(msg.value);
+
+    }
+
+
+    function withdraw(uint money) public{
+
+
+        require(money <= accounts[msg.sender]);
+
+        accounts[msg.sender] = accounts[msg.sender].sub(money);
+    }
+}
