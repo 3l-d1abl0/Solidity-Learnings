b877311ec5735118e36b430d3d30cd3ad9f86073
sameer <sameer.barha12@gmail.com>
2022-01-04 19:25:25 +0530

    update require examples

--- 13-require.sol
+++ 13-require.sol
@@ -1,26 +1,24 @@
 pragma solidity ^0.8.0;
 
-
 /*
 Require - Reverts if the condition is not met:
     - validate response from an external contract
     - validate state condition prior to executing state changing operation
     - used towars teh beginning of the function
  */
-contract Bank{
+contract Bank {
+    mapping(address => uint256) public accounts;
 
-    mapping(address => uint) public accounts;
-
-    function deposit() public payable{
-
+    function deposit() public payable {
         //reverts with an error message if condition not met
-        require(accounts[msg.sender] + msg.value >= accounts[msg.sender], "Overflow error");
-        accounts[msg.sender] +=msg.value;
+        require(
+            accounts[msg.sender] + msg.value >= accounts[msg.sender],
+            "Overflow error"
+        );
+        accounts[msg.sender] += msg.value;
     }
 
-
-    function withdraw(uint money) public{
-
+    function withdraw(uint256 money) public {
         require(money <= accounts[msg.sender]);
         accounts[msg.sender] -= money;
     }
@@ -31,33 +29,25 @@ undo all changes made
 if-else logic Flow
 will refund the remaing gas */
 
-contract BnakRevert{
+contract BnakRevert {
+    mapping(address => uint256) public accounts;
 
-    mapping(address => uint) public accounts;
-
-
-    function deposit(uint money)public payable{
-
-        if(accounts[msg.sender] + msg.value >= accounts[msg.sender]){
+    function deposit(uint256 money) public payable {
+        if (accounts[msg.sender] + msg.value >= accounts[msg.sender]) {
             revert("Overflow error");
         }
 
         accounts[msg.sender] += money;
     }
 
-
-    function withdraw(uint money) public{
-
-        if(money <= accounts[msg.sender]){
+    function withdraw(uint256 money) public {
+        if (money <= accounts[msg.sender]) {
             revert();
         }
 
         accounts[msg.sender] -= money;
-
     }
 }
-
-
 
 /*Assert
     - uses all of the remaining Gas
@@ -65,24 +55,21 @@ contract BnakRevert{
     - check for underflow/overflow
     - validates states after making changes
 */
-contract MathAssert{
-
-    function add(uint256 a, uint256 b) internal pure returns(uint){
-
-        uint c = a+b;
+contract MathAssert {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
         assert(c >= a);
         return c;
     }
 
-    function multiply(uint256 a, uint256 b) internal pure returns (uint){
-        
-        if(a==0){
+    function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
             return 0;
         }
 
-        uint c = a*b;
+        uint256 c = a * b;
 
-        assert(c/a ==b);
+        assert(c / a == b);
         return c;
     }
 }
