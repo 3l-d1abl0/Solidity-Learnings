0ea2e7f1cfdea940cdec6bc9af853a6370d4369f
sameer <sameer.barha12@gmail.com>
2022-07-07 21:53:06 +0530

    added test to approve and transfer from

--- projects/03-erc20-token/test/ERC20-implementation.js
+++ projects/03-erc20-token/test/ERC20-implementation.js
@@ -38,61 +38,90 @@ contract("TruffleContract", (accounts) => {
     //console.log(await this.testContract.symbol());
     //return assert.isTrue(true);
       
+  });
+
+  it("should check balance transfer", async() =>{
+
+    let tokensA = 600;
+    let receipt = await erc20.transfer(accountA, tokensA);
+    //Check for Transfer event for account A
+    expectEvent(receipt, "Transfer", {
+      from: owner_address,
+      to: accountA,
+      tokens: web3.utils.toBN(tokensA),
+    });
+    
+    let tokensB = 500;
+    receipt = await erc20.transfer(accountB, tokensB);
+    //Check for Transfer event for account B
+    expectEvent(receipt, "Transfer", {
+      from: owner_address,
+      to: accountB,
+      tokens: web3.utils.toBN(tokensB),
     });
 
-    it("should check balance transfer", async() =>{
+    //A balance should be equal to tokensA
+    const balanceA = await erc20.balanceOf(accountA);
+    assert.equal(balanceA, tokensA);
+    
+    //B balance should be equal to tokensB
+    const balanceB = await erc20.balanceOf(accountB);
+    assert.equal(balanceB, tokensB);
 
-      let tokensA = 600;
-      let receipt = await erc20.transfer(accountA, tokensA);
-      //Check for Transfer event for account A
-      expectEvent(receipt, "Transfer", {
-        from: owner_address,
-        to: accountA,
-        tokens: web3.utils.toBN(tokensA),
-      });
-      
-      let tokensB = 500;
-      receipt = await erc20.transfer(accountB, tokensB);
-      //Check for Transfer event for account B
-      expectEvent(receipt, "Transfer", {
-        from: owner_address,
-        to: accountB,
-        tokens: web3.utils.toBN(tokensB),
-      });
+  });
 
-      //A balance should be equal to tokensA
-      const balanceA = await erc20.balanceOf(accountA);
-      assert.equal(balanceA, tokensA);
-      
-      //B balance should be equal to tokensB
-      const balanceB = await erc20.balanceOf(accountB);
-      assert.equal(balanceB, tokensB);
+  it("should not transfer without approval", async()=>{
 
+      //Has accountA allowed owner to transfer on its behalf ?
+      allowance = await erc20.allowance(accountA, owner_address);
+      //console.log(allowance);
+      assert(allowance.isZero(), "Allowance should be zero");
+
+      let tokens = 300;
+      //expecct a revert since accountA has not approved owner_account to transfer token on its behalf
+      await expectRevert(
+        erc20.transferFrom(accountA, accountB, tokens),
+        "allowance too low",
+      );
+
+  });
+    
+  it("should aprove owner_account as a spender for accountA", async ()=>{
+
+    let allowance = 500;
+    
+    //accountA allowing owner to be spender on its behalf
+    receipt = await erc20.approve(owner_address, allowance, { from: accountA });
+    expectEvent(receipt, "Approval", {
+      tokenOwner: accountA,
+      spender: owner_address,
+      tokens: web3.utils.toBN(allowance),
     });
 
-    it("should not transfer without approval", async()=>{
-        allowance = await erc20.allowance(accountA, owner_address);
-        //console.log(allowance);
-        assert(allowance.isZero(), "Allowance should be zero");
+    const allowedByA = await erc20.allowance(accountA, owner_address);
+    assert.equal(allowance,allowedByA, "Allowance Mismatch");
 
-        let tokens = 300;
-        //expecct a revert since accountA has not approved owner_account to transfer token on its behalf
-        await expectRevert(
-          erc20.transferFrom(accountA, accountB, tokens),
-          "allowance too low",
-        );
-
-        //receipt = await erc20.approve(accounts[1], value); //accounts[0] has approved accounts[1] for 100 tokens
-        //allowance = await token.allowance(accounts[0], accounts[1]); //allowance =100
-        //assert(allowance.eq(value));
-
+    //Transfer token A to B by owner
+    let transferAtoB = 500;
+    receipt = await erc20.transferFrom(accountA, accountB, transferAtoB);
+    //Expect a Transfer Event
+    expectEvent(receipt, "Transfer", {
+      from: accountA,
+      to: accountB,
+      tokens: web3.utils.toBN(transferAtoB),
     });
-    /*
-    it("should aprove owner_account as a spender for accountA", async ()=>{
 
-      let AtoB =200;
-      let receipt = await erc20.transfer(accountB, AtoB, { from: accountA });
-    });
-    */
+
+    //A balance should be equal to 100
+    const balanceA = await erc20.balanceOf(accountA);
+    assert.equal(balanceA, 100);
+    
+    //B balance should be equal to 1000
+    const balanceB = await erc20.balanceOf(accountB);
+    assert.equal(balanceB, 1000);
+
+
+  });
+    
 
 });
